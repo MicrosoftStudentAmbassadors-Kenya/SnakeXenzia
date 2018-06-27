ee813347bd6a0818c198cb853c6314720c88f051
Josh Cardif <josh.cardif14@outlook.com>
2018-06-27 18:23:21 +0300

    begin

--- SnakeXenzia/Program.cs
+++ SnakeXenzia/Program.cs
@@ -4,9 +4,12 @@ namespace SnakeXenzia
 {
     class Program
     {
+        static char [][] grid=new char[20][];
+        static int width=50;
+        static int height=20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
