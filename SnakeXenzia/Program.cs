0b2c4603443a0a88c1d61f5021a33539df27d0fc
Josh Cardif <josh.cardif14@outlook.com>
2018-06-27 18:40:26 +0300

    init frame

--- SnakeXenzia/Program.cs
+++ SnakeXenzia/Program.cs
@@ -9,7 +9,33 @@ namespace SnakeXenzia
         static int height=20;
         static void Main(string[] args)
         {
-            
+            InitFrame();
+        }
+
+        private static void InitFrame()
+        {
+           Console.CursorVisible=false;
+           for(int i=0;i<height;i++)
+             grid[i]=new char[width];
+
+           grid[0][0]='0';
+           grid[0][width-1]='0';
+           grid[height-1][0]='0';
+           grid[height-1][width-1]='0';
+
+           for(int i=1;i<height-1;i++)
+           {
+               grid[i][0]='|';
+               grid[i][width-1]='|';
+           }
+           for(int i=1;i<width-1;i++)
+           {
+               grid[i][0]='=';
+               grid[i][height-1]='=';
+           } 
+           for(int i=1;i<height-1;i++)
+            for(int c=1;c<width-1;c++)
+             grid[i][c]=' ';
         }
     }
 }
